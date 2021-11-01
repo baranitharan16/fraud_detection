9b76a886d0266c14559b546821ef5aebc268daa8
baranitharan16 <baranitharan.m9@gmail.com>
2021-11-02 01:05:03 +0530

    Code for new ML Models added in the file

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,13 @@
 
 
 print("Adding all ML related code here")
+
+
+def model1():
+    print("adding ML Model 1 code here")
+
+def model2():
+    print("adding ML Model 2 code here")
+
+def model3():
+    print("adding ML Model 3 code here")
